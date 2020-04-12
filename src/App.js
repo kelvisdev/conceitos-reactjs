cda47c66b5484b262307d4fd3499aefe9c90fdc5
Kelvis Borges <kelvisbcc@gmail.com>
2020-04-12 19:49:30 -0300

    Add, List and Remove Repositories

--- src/App.js
+++ src/App.js
@@ -1,29 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
+
+import { FiHeart, FiTrash2, FiGithub } from "react-icons/fi";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      console.log(response.data);
+
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `Repository ${Date.now()}`,
+      url: "https://github.com/kelvisdev/conceitos-reactjs",
+      techs: ["Node.js", "React.js", "React Native"],
+    });
+
+    const { data } = response;
+
+    setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+
+      setRepositories(
+        repositories.filter((repository) => repository.id !== id)
+      );
+    } catch (error) {
+      alert("Error removing repository, try again.");
+    }
+  }
+
+  async function handleUrlRepository(url) {
+    window.open(
+      `${url}`,
+      '_blank'
+    );
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <div className="repository-container">
+      <section>
+        <ul data-testid="repository-list">
+          {repositories.map((repository) => (
+            <li key={repository.id}>
+              <h3>{repository.title}</h3>
+              {repository.techs.map((tech) => (
+                <span key={tech}>{tech}</span>
+              ))}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+              <div>
+                <button className="btn-like">
+                  <FiHeart size={14} color="#ca4949" /> {repository.likes}{" "}
+                  <small>Curtidas</small>
+                </button>
+              </div>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+              <div>
+                <button
+                  className="btn-remove"
+                  onClick={() => handleRemoveRepository(repository.id)}
+                >
+                  <p className="title-btn">Remover</p> <FiTrash2 size={20} color="#fff" />
+                </button>
+
+                <button
+                  className="btn-url"
+                  onClick={() => handleUrlRepository(repository.url)}
+                >
+                  <p className="title-btn">Repositório </p> <FiGithub size={20} color="#fff" />                  
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        <button className="btn-add" onClick={handleAddRepository}>Adicionar</button>
+      </section>
     </div>
   );
 }
